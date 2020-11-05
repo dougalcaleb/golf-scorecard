8fef76a70dea0c02f73bb1eb4614c1fd74b086fa
dougalcaleb <dougalcaleb@gmail.com>
2020-11-05 09:24:48 -0700

    Generalized scorecard generation. Pre-cleanup

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,7 @@ let course;
 let apiurl = "";
 let courseData;
 let courses = {};
-let activeCourse;
+let activeCourse, teecount = 0;
 
 let colpos = 0, aligned = false;
 
@@ -168,7 +168,14 @@ function generateScorecard() {
     let newcol, newcell;
     let databody = document.querySelector(".databody");
     let oncol = 0;
-    let teeCount = activeCourse.data.holes[0].teeBoxes.length-1;
+    // let teeCount = activeCourse.data.holes[0].teeBoxes.length-1;
+    if (activeCourse.data.holes[0].teeBoxes[activeCourse.data.holes[1].teeBoxes.length-1].teeType === "auto change location") {
+        teeCount = activeCourse.data.holes[0].teeBoxes.length-1;
+    } else {
+        teeCount = activeCourse.data.holes[0].teeBoxes.length;
+    }
+    // teeCount = (activeCourse.data.holes[0].teeBoxes[activeCourse.data.holes[1].teeBoxes.length-1].teeHexColor = null) ? activeCourse.data.holes[0].teeBoxes.length-2 : activeCourse.data.holes[0].teeBoxes.length-1;
+    console.log(teeCount+" tees");
     let playerCount = 4;
     let totalRowCount = 3 + playerCount+teeCount;
     // FIRST 9
@@ -191,8 +198,8 @@ function generateScorecard() {
             newcell = document.createElement("DIV");
             newcell.classList.add("r"+(aa+1));
             newcol.appendChild(newcell);
-            newcell.style.background = activeCourse.data.holes[0].teeBoxes[aa].teeHexColor;
-            newcell.style.color =getDynamicColor(activeCourse.data.holes[0].teeBoxes[aa].teeHexColor);
+            newcell.style.background = activeCourse.data.holes[1].teeBoxes[aa].teeHexColor;
+            newcell.style.color =getDynamicColor(activeCourse.data.holes[1].teeBoxes[aa].teeHexColor);
         }
         for (let ab = 0; ab < 4; ab++) {
             newcell = document.createElement("DIV");
@@ -223,8 +230,9 @@ function generateScorecard() {
         } else {
             newcell.innerText = "--";
         }
-        if (b > 0 && b < 5) {
-            newcell.style.background = getMutedColor(activeCourse.data.holes[0].teeBoxes[b-1].teeHexColor);
+        if (b > 0 && b <= teeCount) {
+            newcell.style.background = getMutedColor(activeCourse.data.holes[1].teeBoxes[b-1].teeHexColor);
+            newcell.style.color = getDynamicColor(activeCourse.data.holes[1].teeBoxes[b-1].teeHexColor);
         }
     }
     // SECOND 9
@@ -240,15 +248,15 @@ function generateScorecard() {
         
         newcell = document.createElement("DIV");
         newcell.classList.add("r0");
-        newcell.innerText = (c+1);
+        newcell.innerText = (c+10);
         newcol.appendChild(newcell);
 
         for (let ca = 0; ca < teeCount; ca++) {
             newcell = document.createElement("DIV");
             newcell.classList.add("r"+(ca+1));
             newcol.appendChild(newcell);
-            newcell.style.background = activeCourse.data.holes[0].teeBoxes[ca].teeHexColor;
-            newcell.style.color =getDynamicColor(activeCourse.data.holes[0].teeBoxes[ca].teeHexColor);
+            newcell.style.background = activeCourse.data.holes[1].teeBoxes[ca].teeHexColor;
+            newcell.style.color = getDynamicColor(activeCourse.data.holes[1].teeBoxes[ca].teeHexColor);
         }
         for (let cb = 0; cb < 4; cb++) {
             newcell = document.createElement("DIV");
@@ -270,14 +278,18 @@ function generateScorecard() {
     databody.appendChild(newcol);
     newcol.style.left = (oncol*26)+"vw";
     oncol++;
-    for (let f = 0; f < 11; f++) {
+    for (let d = 0; d < 11; d++) {
         newcell = document.createElement("DIV");
-        newcell.classList.add("r"+f);
+        newcell.classList.add("r"+d);
         newcol.appendChild(newcell);
-        if (f === 0) {
+        if (d === 0) {
             newcell.innerText = "IN";
         } else {
             newcell.innerText = "--";
+        }
+        if (d > 0 && d <= teeCount) {
+            newcell.style.background = getMutedColor(activeCourse.data.holes[1].teeBoxes[d-1].teeHexColor);
+            newcell.style.color = getDynamicColor(activeCourse.data.holes[1].teeBoxes[d-1].teeHexColor);
         }
     }
     // TOTAL COL
@@ -286,14 +298,18 @@ function generateScorecard() {
     databody.appendChild(newcol);
     newcol.style.left = (oncol*26)+"vw";
     oncol++;
-    for (let g = 0; g < 11; g++) {
+    for (let e = 0; e < 11; e++) {
         newcell = document.createElement("DIV");
-        newcell.classList.add("r"+g);
+        newcell.classList.add("r"+e);
         newcol.appendChild(newcell);
-        if (g === 0) {
+        if (e === 0) {
             newcell.innerText = "TOTAL";
         } else {
             newcell.innerText = "--";
+        }
+        if (e > 0 && e <= teeCount) {
+            newcell.style.background = activeCourse.data.holes[1].teeBoxes[e-1].teeHexColor;
+            newcell.style.color = getDynamicColor(activeCourse.data.holes[1].teeBoxes[e-1].teeHexColor);
         }
     }
     // HCP COL
@@ -302,15 +318,19 @@ function generateScorecard() {
     databody.appendChild(newcol);
     newcol.style.left = (oncol*26)+"vw";
     oncol++;
-    for (let h = 0; h < 11; h++) {
+    for (let f = 0; f < 11; f++) {
         newcell = document.createElement("DIV");
-        newcell.classList.add("r"+h);
+        newcell.classList.add("r"+f);
         newcol.appendChild(newcell);
-        if (h === 0) {
+        if (f === 0) {
             newcell.innerText = "HCP";
         } else {
             newcell.innerText = "--";
         }
+        // if (f > 0 && f <= teecount) {
+        //     newcell.style.background = activeCourse.data.holes[1].teeBoxes[f-1].teeHexColor;
+        //     newcell.style.color = getDynamicColor(activeCourse.data.holes[1].teeBoxes[f-1].teeHexColor);
+        // }
     }
     // NET COL
     newcol = document.createElement("DIV");
@@ -318,35 +338,57 @@ function generateScorecard() {
     databody.appendChild(newcol);
     newcol.style.left = (oncol*26)+"vw";
     oncol++;
-    for (let i = 0; i < 11; i++) {
+    for (let g = 0; g < 11; g++) {
         newcell = document.createElement("DIV");
-        newcell.classList.add("r"+i);
+        newcell.classList.add("r"+g);
         newcol.appendChild(newcell);
-        if (i === 0) {
+        if (g === 0) {
             newcell.innerText = "NET";
         } else {
             newcell.innerText = "--";
         }
+        // if (g > 0 && g <= teecount) {
+        //     newcell.style.background = activeCourse.data.holes[1].teeBoxes[g-1].teeHexColor;
+        //     newcell.style.color = getDynamicColor(activeCourse.data.holes[1].teeBoxes[g-1].teeHexColor);
+        // }
     }
 }
 
 function fillCard(id) {
     activeCourse = JSON.parse(sessionStorage.getItem(activeCourse));
     generateScorecard();
-    let total0 = 0, total1 = 0, total2 = 0, total3 = 0;
-    let gtotal0 = 0, gtotal1 = 0, gtotal2 = 0, gtotal3 = 0;
+    // let total0 = 0, total1 = 0, total2 = 0, total3 = 0;
+    // let gtotal0 = 0, gtotal1 = 0, gtotal2 = 0, gtotal3 = 0;
+    let totals = [];
+    let gtotals = [];
     let hcptotal = 0, partotal = 0;
     let hcpgtotal = 0, pargtotal = 0;
+    for (let pre = 0; pre < teeCount; pre++) {
+        totals.push(0);
+        gtotals.push(0);
+    }
     for (let a = 0; a < 9; a++) {
-        document.querySelector(".data-col-"+a).querySelector(".r1").innerText = activeCourse.data.holes[a].teeBoxes[1].yards;
-        document.querySelector(".data-col-"+a).querySelector(".r2").innerText = activeCourse.data.holes[a].teeBoxes[3].yards;
-        document.querySelector(".data-col-"+a).querySelector(".r3").innerText = activeCourse.data.holes[a].teeBoxes[0].yards;
-        document.querySelector(".data-col-"+a).querySelector(".r4").innerText = activeCourse.data.holes[a].teeBoxes[2].yards;
+        // if (activeCourse.data.holes[a].teeBoxes[1].yards) {
+        //     document.querySelector(".data-col-"+a).querySelector(".r1").innerText = activeCourse.data.holes[a].teeBoxes[1].yards;
+        //     total0 += activeCourse.data.holes[a].teeBoxes[1].yards;
+        // }
+        // if (activeCourse.data.holes[a].teeBoxes[3].yards) {
+        //     document.querySelector(".data-col-"+a).querySelector(".r2").innerText = activeCourse.data.holes[a].teeBoxes[3].yards;
+        //     total1 += activeCourse.data.holes[a].teeBoxes[3].yards;
+        // }
+        // if (activeCourse.data.holes[a].teeBoxes[0].yards) {
+        //     document.querySelector(".data-col-"+a).querySelector(".r3").innerText = activeCourse.data.holes[a].teeBoxes[0].yards;
+        //     total2 += activeCourse.data.holes[a].teeBoxes[0].yards;
+        // }
+        // if (activeCourse.data.holes[a].teeBoxes[2].yards) {
+        //     document.querySelector(".data-col-"+a).querySelector(".r4").innerText = activeCourse.data.holes[a].teeBoxes[2].yards;
+        //     total3 += activeCourse.data.holes[a].teeBoxes[2].yards;
+        // }
 
-        total0 += activeCourse.data.holes[a].teeBoxes[1].yards;
-        total1 += activeCourse.data.holes[a].teeBoxes[3].yards;
-        total2 += activeCourse.data.holes[a].teeBoxes[0].yards;
-        total3 += activeCourse.data.holes[a].teeBoxes[2].yards;
+        for (let aa = 0; aa < teeCount; aa++) {
+            document.querySelector(".data-col-"+a).querySelector(".r"+(aa+1)).innerText = activeCourse.data.holes[a].teeBoxes[aa].yards;
+            totals[aa] += activeCourse.data.holes[a].teeBoxes[aa].yards;
+        }
 
         document.querySelector(".data-col-"+a).querySelector(".r9").innerText = activeCourse.data.holes[a].teeBoxes[0].par;
         document.querySelector(".data-col-"+a).querySelector(".r10").innerText = activeCourse.data.holes[a].teeBoxes[0].hcp;
@@ -354,55 +396,85 @@ function fillCard(id) {
         // hcptotal += activeCourse.data.holes[a].teeBoxes[0].hcp;     hcpgtotal += hcptotal;
         partotal += activeCourse.data.holes[a].teeBoxes[0].par;
     }
-    gtotal0 += total0;
-    gtotal1 += total1;
-    gtotal2 += total2;
-    gtotal3 += total3;
+
+    console.log("totals for section 1 are "+totals);
+
+    // gtotal0 += total0;
+    // gtotal1 += total1;
+    // gtotal2 += total2;
+    // gtotal3 += total3;
     pargtotal += partotal;
 
-    document.querySelector(".data-col-OUT").querySelector(".r1").innerText = total0;
-    document.querySelector(".data-col-OUT").querySelector(".r2").innerText = total1;
-    document.querySelector(".data-col-OUT").querySelector(".r3").innerText = total2;
-    document.querySelector(".data-col-OUT").querySelector(".r4").innerText = total3;
-
-    document.querySelector(".data-col-OUT").querySelector(".r9").innerText = partotal;
-
-    total0 = 0; total1 = 0; total2 = 0; total3 = 0; partotal = 0;
-    for (let b = 0; b < 9; b++) {
-        document.querySelector(".data-col-"+(b+9)).querySelector(".r1").innerText = activeCourse.data.holes[b+9].teeBoxes[1].yards;
-        document.querySelector(".data-col-"+(b+9)).querySelector(".r2").innerText = activeCourse.data.holes[b+9].teeBoxes[3].yards;
-        document.querySelector(".data-col-"+(b+9)).querySelector(".r3").innerText = activeCourse.data.holes[b+9].teeBoxes[0].yards;
-        document.querySelector(".data-col-"+(b+9)).querySelector(".r4").innerText = activeCourse.data.holes[b+9].teeBoxes[2].yards;
-
-        total0 += activeCourse.data.holes[b+9].teeBoxes[1].yards;
-        total1 += activeCourse.data.holes[b+9].teeBoxes[3].yards;
-        total2 += activeCourse.data.holes[b+9].teeBoxes[0].yards;
-        total3 += activeCourse.data.holes[b+9].teeBoxes[2].yards;
-
-        document.querySelector(".data-col-"+(b+9)).querySelector(".r9").innerText = activeCourse.data.holes[b+9].teeBoxes[0].par;
-        document.querySelector(".data-col-"+(b+9)).querySelector(".r10").innerText = activeCourse.data.holes[b+9].teeBoxes[0].hcp;
-
-        partotal += activeCourse.data.holes[b].teeBoxes[0].par;
+    for (let b = 0; b < teeCount; b++) {
+        document.querySelector(".data-col-OUT").querySelector(".r"+(b+1)).innerText = totals[b];
+        gtotals[b] += totals[b];
     }
-    gtotal0 += total0;
-    gtotal1 += total1;
-    gtotal2 += total2;
-    gtotal3 += total3;
+
+    document.querySelector(".data-col-OUT").querySelector(".r"+(5+teeCount)).innerText = partotal;
+
+    // total0 = 0; total1 = 0; total2 = 0; total3 = 0; 
+
+    for (let c = 0; c < totals.length; c++) {
+        totals[c] = 0;
+    }
+
+    partotal = 0;
+
+    for (let d = 0; d < 9; d++) {
+        // document.querySelector(".data-col-"+(b+9)).querySelector(".r1").innerText = activeCourse.data.holes[b+9].teeBoxes[1].yards;
+        // document.querySelector(".data-col-"+(b+9)).querySelector(".r2").innerText = activeCourse.data.holes[b+9].teeBoxes[3].yards;
+        // document.querySelector(".data-col-"+(b+9)).querySelector(".r3").innerText = activeCourse.data.holes[b+9].teeBoxes[0].yards;
+        // document.querySelector(".data-col-"+(b+9)).querySelector(".r4").innerText = activeCourse.data.holes[b+9].teeBoxes[2].yards;
+
+        // total0 += activeCourse.data.holes[b+9].teeBoxes[1].yards;
+        // total1 += activeCourse.data.holes[b+9].teeBoxes[3].yards;
+        // total2 += activeCourse.data.holes[b+9].teeBoxes[0].yards;
+        // total3 += activeCourse.data.holes[b+9].teeBoxes[2].yards;
+
+
+        for (let da = 0; da < teeCount; da++) {
+            document.querySelector(".data-col-"+(d+9)).querySelector(".r"+(da+1)).innerText = activeCourse.data.holes[d+9].teeBoxes[da].yards;
+            totals[da] += activeCourse.data.holes[d+9].teeBoxes[da].yards;
+        }
+
+        document.querySelector(".data-col-"+(d+9)).querySelector(".r9").innerText = activeCourse.data.holes[d+9].teeBoxes[0].par;
+        document.querySelector(".data-col-"+(d+9)).querySelector(".r10").innerText = activeCourse.data.holes[d+9].teeBoxes[0].hcp;
+
+        partotal += activeCourse.data.holes[d].teeBoxes[0].par;
+    }
+
+    console.log("totals for section 2 are "+totals);
+
+
+    // gtotal0 += total0;
+    // gtotal1 += total1;
+    // gtotal2 += total2;
+    // gtotal3 += total3;
     pargtotal += partotal;
 
-    document.querySelector(".data-col-IN").querySelector(".r1").innerText = total0;
-    document.querySelector(".data-col-IN").querySelector(".r2").innerText = total1;
-    document.querySelector(".data-col-IN").querySelector(".r3").innerText = total2;
-    document.querySelector(".data-col-IN").querySelector(".r4").innerText = total3;
+    for (let e = 0; e < teeCount; e++) {
+        document.querySelector(".data-col-IN").querySelector(".r"+(e+1)).innerText = totals[e];
+        gtotals[e] += totals[e];
+        document.querySelector(".data-col-TOT").querySelector(".r"+(e+1)).innerText = gtotals[e];
+    }
 
-    document.querySelector(".data-col-IN").querySelector(".r9").innerText = partotal;
+    // document.querySelector(".data-col-IN").querySelector(".r1").innerText = total0;
+    // document.querySelector(".data-col-IN").querySelector(".r2").innerText = total1;
+    // document.querySelector(".data-col-IN").querySelector(".r3").innerText = total2;
+    // document.querySelector(".data-col-IN").querySelector(".r4").innerText = total3;
 
-    document.querySelector(".data-col-TOT").querySelector(".r1").innerText = gtotal0;
-    document.querySelector(".data-col-TOT").querySelector(".r2").innerText = gtotal1;
-    document.querySelector(".data-col-TOT").querySelector(".r3").innerText = gtotal2;
-    document.querySelector(".data-col-TOT").querySelector(".r4").innerText = gtotal3;
+    document.querySelector(".data-col-IN").querySelector(".r"+(5+teeCount)).innerText = partotal;
 
-    document.querySelector(".data-col-TOT").querySelector(".r9").innerText = pargtotal;
+    // for (let e = 0; e < teecount; e++) {
+    //     document.querySelector(".data-col-TOT").querySelector(".r1").innerText = gtotal0;
+    // }
+
+    // document.querySelector(".data-col-TOT").querySelector(".r1").innerText = gtotal0;
+    // document.querySelector(".data-col-TOT").querySelector(".r2").innerText = gtotal1;
+    // document.querySelector(".data-col-TOT").querySelector(".r3").innerText = gtotal2;
+    // document.querySelector(".data-col-TOT").querySelector(".r4").innerText = gtotal3;
+
+    document.querySelector(".data-col-TOT").querySelector(".r"+(5+teeCount)).innerText = pargtotal;
 
     setTimeout(() => {
         window.scroll({
